411d682fafb53b9cc0e72bd0e0250b31e0edaf20
Ethan Nolan <ethan.nolan@my.jcu.edu.au>
2024-07-23 22:27:47 +1000

    Installed SDL2

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 #include <vector>
 #include <string>
 #include "refraction.h"
@@ -6,27 +7,28 @@
 #include "layerMaterial.h"
 
 #include <SDL2/SDL.h>
-#include <stdio.h>
 
 #define SCREEN_WIDTH 1280 
 #define SCREEN_HEIGHT 720
 
 using namespace std;
 
-HitInfo intersection(const Ray& ray, const layer){ // Simplification assuming that all layers are horizontal.
-    // take big steps (1, 2, 3)
-    // when above line take steps back (2.5)
+// HitInfo intersection(const Ray& ray, const layer){ // Simplification assuming that all layers are horizontal.
+//     // take big steps (1, 2, 3)
+//     // when above line take steps back (2.5)
     
-}
+// }
 
-int main(){
+int WinMain(){
     float msg = refractedAngle(1.1, 1, 40*PI/180) * 180 / (PI);
     cout << msg << " ";
 
     Ray testRay(Vector2(1, 2), Vector2(0, 1)); // vector at 1, 2 pointing directly up
     layer TestLayer(1, [](double x) {return 4; }); // layer with refractive index 1 at y=4;
 
+    cout << "\nend\n";
 
+    
 
     return 0;
 }
