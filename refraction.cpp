e94a7cb71eb00dce4b6efa09a68200898a4115ea
Ethan Nolan <ethan.nolan@my.jcu.edu.au>
2024-07-31 15:05:39 +1000

    Update refraction code to handle differing angles

--- refraction.cpp
+++ refraction.cpp
@@ -1,8 +1,19 @@
 #include "refraction.h"
 
-float refractedAngle(float n1, float n2, float angle1){
-    if ((n1 > n2) && (angle1 > asin(n2/n1))){
-        return (-1*angle1);    
+float refractedAngle(float n1, float n2, ray Ray, Vector2 normal);{
+
+    float dotProduct = ray.direction.dot(normal);
+    float angleWithNormal = acos(dotProduct);
+
+    if ((n1 > n2) && (angleWithNormal > asin(n2 / n1))) {
+        refractedAngleWithNormal = -angleWithNormal;
+    } else {
+        refractedAngleWithNormal = asin(n1 / n2 * sin(angleWithNormal));
     }
-    return asin(n1/n2 * sin(angle1));
+
+    // Convert the refracted angle back to the global coordinate system
+    float globalRefractedAngle = atan2(sin(incidentAngle1) * cos(refractedAngleWithNormal) - cos(incidentAngle1) * sin(refractedAngleWithNormal), 
+                                       cos(incidentAngle1) * cos(refractedAngleWithNormal) + sin(incidentAngle1) * sin(refractedAngleWithNormal));
+
+    return globalRefractedAngle; // return global angle
 }
--- refraction.h
+++ refraction.h
@@ -2,9 +2,11 @@
 #define REFRACTEDANGLE_H
 
 #include <cmath>
+#include "Vector2.h"
+#include "ray2.h"
 
 #define PI acos(-1)
 
-float refractedAngle(float n1, float n2, float angle1);
+float refractedAngle(float n1, float n2, ray Ray, Vector2 normal);
 
 #endif
