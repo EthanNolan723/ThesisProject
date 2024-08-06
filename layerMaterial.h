38df2d28732a9ba25ab8b527caad91d25edd498b
Ethan Nolan <ethan.nolan@my.jcu.edu.au>
2024-08-06 19:16:32 +1000

    Add .draw Functiononality of layers

--- layerMaterial.h
+++ layerMaterial.h
@@ -7,14 +7,14 @@
 using namespace std;
 
 struct Layer{
-    private:
+private:
     vector<Vector2> points;
     float refractiveIndex;
     // reflection
-    // flip sides
+    // flip sides (left right for inf size)
     // absorption
 
-    public:
+public:
     // Constructors
     Layer() : points(), refractiveIndex(1) {}
     Layer(float refrIndex, const vector<Vector2>& pts)
@@ -27,6 +27,16 @@ struct Layer{
     // Method to add a point
     void addPoint(const Vector2& point) {
         points.push_back(point);
+    }
+
+    void draw(SDL_Renderer* renderer) const {
+        // Set the color for drawing (here, we choose white, but you can change it)
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        // Draw lines between consecutive points
+        for (size_t i = 0; i < points.size() - 1; ++i) {
+            SDL_RenderDrawLine(renderer, points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+        }
     }
 };
 
