4124c91c8c4cb6e09f1c1163926b1caa5dca2575
Ethan Nolan <ethan.nolan@my.jcu.edu.au>
2024-08-06 19:14:53 +1000

    Fix global refraction angle error

--- refraction.cpp
+++ refraction.cpp
@@ -1,22 +1,30 @@
 #include "refraction.h"
 
 Vector2 refractedAngle(float n1, float n2, Ray ray, Vector2 normal){
-    // Ensure the normal is pointing in the correct direction
-    if (ray.getDirection().dot(normal) < 0) {
-        normal = -normal;
-    }
     
-    float angleWithNormal = acos(ray.getDirection().dot(normal) / (ray.getDirection().length() * normal.length()));
+    double dotProduct = ray.getDirection().dot(normal);
+    // Ensure the normal is pointing in the correct direction
+    if (dotProduct == 1 || dotProduct == -1) // Pointing exact same direction
+        return ray.getDirection();
+    else if (dotProduct < 0){ // Correct Normal Direction
+        normal = -normal;
+        dotProduct = ray.getDirection().dot(normal);
+    }
+
+    float angleWithNormal = acos(dotProduct / (ray.getDirection().length() * normal.length()));
 
     float refractedAngleWithNormal;
-    if ((n1 > n2) && (angleWithNormal > asin(n2 / n1))) {
+    if ((n1 > n2) && (angleWithNormal > asin(n2 / n1))) // Total Internal Reflection
         refractedAngleWithNormal = -angleWithNormal;
-    } else {
+    else
         refractedAngleWithNormal = asin(n1 / n2 * sin(angleWithNormal));
-    }
 
-    // Convert the refracted angle back to the global coordinate system
-    float globalRefractedAngle = normal.angle() - refractedAngleWithNormal;
-
-    return Vector2(cos(globalRefractedAngle),sin(globalRefractedAngle)); // return global angle
+    // Converting to Global Co-ords
+    float globalRefractedAngle;
+    if (ray.getDirection().cross(normal) > 0) 
+        globalRefractedAngle = normal.angle() - refractedAngleWithNormal;
+    else
+        globalRefractedAngle =  normal.angle() + refractedAngleWithNormal; 
+    
+    return Vector2(cos(globalRefractedAngle),sin(globalRefractedAngle)); // return global angle vect
 }
