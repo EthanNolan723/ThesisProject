ddfa53cc3eeb976407f810340950d4f328ce488f
Ethan Nolan <ethan.nolan@my.jcu.edu.au>
2024-08-06 19:16:03 +1000

    Add simple collision detection of rays

--- ray.h
+++ ray.h
@@ -2,20 +2,25 @@
 #define RAY_H
 
 #include "Vector2.h"
+#include <vector>
 #include <SDL2/SDL.h>
+
+using namespace std;
 
 class Ray {
 private:
     Vector2 position;
     Vector2 direction;
+    int bounces;
 public:
     // Constructors
-    Ray() : position(0.0, 0.0), direction(0.0, 0.0) {}
-    Ray(const Vector2& origin, const Vector2& direction) : position(origin), direction(direction.normalized()) {}
+    Ray() : position(0.0, 0.0), direction(0.0, 0.0), bounces(0) {}
+    Ray(const Vector2& origin, const Vector2& direction, const int bounces) : position(origin), direction(direction.normalized()), bounces(bounces) {}
 
     // Accessors
     Vector2 getPos() const { return position; }
     Vector2 getDirection() const { return direction; }
+    int getBounces() const { return bounces; }
 
     // Method to get a point along the ray at parameter t
     Vector2 step(double t) const {
@@ -30,9 +35,11 @@ public:
 
 struct HitInfo{  // If ray hits a object
     bool didHit;
-    float distance;
+    double distance;
     Vector2 hitPoint;
     Vector2 normal;
 };
+
+HitInfo collisionDetectionGOOGOOGAGA(const Ray& ray, const vector<Vector2>& points);
 
 #endif // RAY_H
