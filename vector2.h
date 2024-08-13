3d092b2d62a67b751c313773f321a1f05dbb1c62
Ethan Nolan <ethan.nolan@my.jcu.edu.au>
2024-08-13 18:48:28 +1000

    Add += -= operation support

--- vector2.h
+++ vector2.h
@@ -18,6 +18,9 @@ public:
     Vector2 operator-() const { return Vector2(-x, -y); }
     Vector2 operator*(double t) const { return Vector2(x * t, y * t); }
 
+    Vector2& operator+=(const Vector2& v) { x += v.x; y += v.y; return *this; }
+    Vector2& operator-=(const Vector2& v) { x -= v.x; y -= v.y; return *this; }
+
     double dot(const Vector2& v) const { return x * v.x + y * v.y; }
     double cross(const Vector2& v) const { return x * v.y - y * v.x; }
 
